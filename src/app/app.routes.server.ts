9329aec2b9da657414387db76f6d706625b2d5e0
GoldyRoger07 <latouche822@gmail.com>
2026-06-07 04:56:59 -0400

    83deifdf---

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,6 +5,6 @@ export const serverRoutes: ServerRoute[] = [
   // { path: 'about-us', renderMode: RenderMode.Server },
   {
     path: '**',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Prerender
   }
 ];
